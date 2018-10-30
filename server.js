4da50d91bd42d34e641e45a977f2883392d9dbf5
vphu <vphu@clarity.my>
2018-10-31 01:46:32 +0800

    Added API for add / remove dynamically

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const favicon = require('express-favicon');
 const path = require('path');
 const Sequelize = require('sequelize');
 const port = process.env.PORT || 3000;
+var bodyParser = require('body-parser')
+
 const app = express();
+// parse application/json
+app.use(bodyParser.json())
 app.use(favicon(__dirname + '/build/favicon.png'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -15,7 +19,7 @@ app.get('/ping', function (req, res) {
 
 function getConfiguration() {
   const staff = {
-    "tableName": "Staff",
+    "tableName": "staffs",
     "displayName": "Staff Records",
     "description": "These are your staff records, edit, add, remove and view any details",
     "columns": [
@@ -44,12 +48,12 @@ function getConfiguration() {
   }
 
   const inventory = {
-    "tableName": "Inventory",
+    "tableName": "inventories",
     "displayName": "Inventory Records",
     "description": "These are your Inventory records, edit, add, remove and view any details. Records of what items you have (by batch)",
     "columns": [
       { "name": "inv_id", "display": "Inventory Id", "type": "number", "sequence": "inv_id_seq", "mandatory": true, "unique": true, "key": true },
-      { "name": "inv_prod_id", "display": "Product", "type": "text", "mandatory": true, "ref": "Product" },
+      { "name": "inv_prod_id", "display": "Product", "type": "number", "mandatory": true, "ref": "products" },
       { "name": "inv_bar_code", "display": "Barcode", "type": "barcode", "mandatory": false },
       { "name": "inv_purchase_date", "display": "Purchase Date", "type": "timestamp", "mandatory": true, "default": formatDate() },
       { "name": "inv_expiry_date", "display": "Expiry Date", "type": "timestamp", "mandatory": true },
@@ -59,7 +63,7 @@ function getConfiguration() {
   }
 
   const products = {
-    "tableName": "Product",
+    "tableName": "products",
     "displayName": "Product Records",
     "description": "These are your Product records, edit, add, remove and view any details",
     "columns": [
@@ -67,8 +71,8 @@ function getConfiguration() {
       { "name": "prod_name", "display": "Name", "type": "text", "mandatory": true, "unique": true },
       { "name": "prod_desc", "display": "Description", "type": "text" },
       { "name": "prod_picture", "display": "Picture", "type": "picture" },
-      { "name": "prod_supl_id", "display": "Supplier", "type": "number", "mandatory": true, "ref": "Supplier" },
-      { "name": "prod_cate_id", "display": "Category", "type": "number", "mandatory": true, "ref": "Product_Category" },
+      { "name": "prod_supl_id", "display": "Supplier", "type": "number", "mandatory": true, "ref": "suppliers" },
+      { "name": "prod_cate_id", "display": "Category", "type": "number", "mandatory": true, "ref": "product_categories" },
       { "name": "prod_units_on_order", "display": "Units on Order", "type": "number", "mandatory": true },
       { "name": "prod_discontinued", "display": "Discontinued", "type": "text", "mandatory": true, "lov": ["Discontinued", "In Stock", "Active", "Phase Out"] },
       { "name": "prod_sku", "display": "SKU", "type": "text" },
@@ -78,7 +82,7 @@ function getConfiguration() {
   }
 
   const productCategory = {
-    "tableName": "Product_Category",
+    "tableName": "product_categories",
     "displayName": "Product Categories Records",
     "description": "These are your Product Category records, edit, add, remove and view any details",
     "columns": [
@@ -89,23 +93,23 @@ function getConfiguration() {
   }
 
   const sales = {
-    "tableName": "Sales",
+    "tableName": "sales",
     "displayName": "Sales Records",
     "description": "These are your Sales records, edit, add, remove and view any details",
     "columns": [
       { "name": "sale_id", "display": "Sales Id", "type": "number", "sequence": "sale_id_seq", "mandatory": true, "unique": true, "key": true },
-      { "name": "sale_staff_id", "display": "Staff", "type": "number", "mandatory": true, "unique": true, "ref": "Staff" },
-      { "name": "sale_inv_id", "display": "Inventory", "type": "number", "ref": "Inventory" },
+      { "name": "sale_staff_id", "display": "Staff", "type": "number", "mandatory": true, "unique": true, "ref": "staffs" },
+      { "name": "sale_inv_id", "display": "Inventory", "type": "number", "ref": "inventories" },
       { "name": "sale_price", "display": "Price", "type": "number", "mandatory": true },
       { "name": "sale_cost", "display": "Cost", "type": "number", "mandatory": true },
       { "name": "sale_timestamp", "display": "Sales Date", "type": "timestamp", "mandatory": true },
-      { "name": "sale_cust_id", "display": "Customer", "type": "number", "ref": "Customer" },
+      { "name": "sale_cust_id", "display": "Customer", "type": "number" },
       { "name": "sale_status", "display": "Status", "type": "text", "lov": ["SOLD", "RETURNED", "EXCHANGE", "REFUNDED"] }
     ]
   }
 
   const suppliers = {
-    "tableName": "Suppliers",
+    "tableName": "suppliers",
     "displayName": "Suppliers Records",
     "description": "These are your Supplier records, edit, add, remove and view any details",
     "columns": [
@@ -120,13 +124,13 @@ function getConfiguration() {
   }
 
   const deliveries = {
-    "tableName": "Deliveries",
+    "tableName": "deliveries",
     "displayName": "Delivery Records",
     "description": "These are your Delivery records for your suppliers, edit, add, remove and view any details",
     "columns": [
       { "name": "devy_id", "display": "Delivery Id", "type": "number", "sequence": "devy_id_seq", "mandatory": true, "unique": true, "key": true },
-      { "name": "devy_prod_id", "display": "Product", "type": "number", "mandatory": true, "ref": "Product" },
-      { "name": "devy_supl_id", "display": "Supplier", "type": "number", "mandatory": true, "ref": "Supplier" },
+      { "name": "devy_prod_id", "display": "Product", "type": "number", "mandatory": true, "ref": "products" },
+      { "name": "devy_supl_id", "display": "Supplier", "type": "number", "mandatory": true, "ref": "suppliers" },
       { "name": "devy_request_date", "display": "Request Date", "type": "timestamp", "mandatory": true },
       { "name": "devy_delivery_date", "display": "Delivery Date", "type": "timestamp", "mandatory": true },
       { "name": "devy_status", "display": "Status", "type": "text", "mandatory": true, "lov": ["ON TIME", "LATE", "VERY LATE", "BUSINESS IMPACTING"] },
@@ -135,7 +139,7 @@ function getConfiguration() {
   }
 
   const investments = {
-    "tableName": "Investments",
+    "tableName": "investments",
     "displayName": "Investment Records",
     "description": "These are your Investment records, edit, add, remove and view any details",
     "columns": [
@@ -153,12 +157,13 @@ function getConfiguration() {
 
   const tableConfiguration = [
     { "path": "staff-page", "table": staff },
-    { "path": "inventory-page", "table": inventory },
-    { "path": "products-page", "table": products },
-    { "path": "sales-page", "table": sales },
     { "path": "suppliers-page", "table": suppliers },
+    { "path": "product-catalog", "table": productCategory },
+    { "path": "investments-page", "table": investments },
+    { "path": "products-page", "table": products },
+    { "path": "inventory-page", "table": inventory },
+    { "path": "sales-page", "table": sales },
     { "path": "deliveries-page", "table": deliveries },
-    { "path": "investments-page", "table": investments }
   ]
 
   return tableConfiguration;
@@ -169,7 +174,17 @@ function getConfiguration() {
 var sharedPersistenceMapping = {}
 var persistenceKey = {}
 // function that returns for get / put / update / delete
-var sequelize = null; // connect to sequelize
+var sequelize = new Sequelize('postgres', 'postgres', 'password', {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+}); // connect to sequelize
 
 function deriveType(entry, column) {
   switch (column.type) {
@@ -191,7 +206,7 @@ function deriveType(entry, column) {
   }
 }
 
-function deriveReferences(entry, column) {  
+function deriveReferences(entry, column) {
   if (column.ref) {
     entry.references = {
       model: sharedPersistenceMapping[column.ref.toLowerCase()],
@@ -201,30 +216,52 @@ function deriveReferences(entry, column) {
   }
 }
 
+function deriveDefaults(entry, column) {
+  if (column.default) {
+    if (column.type == "timestamp") {
+      entry.defaultValue = Sequelize.NOW;
+    }
+    else {
+      entry.defaultValue = column.default;
+    }
+  }
+}
+
+/*
+async function run (model, tableName) {
+  try {
+  return await model.sync()
+  }
+  catch (e) {
+    console.log("Failed", tableName, e);
+  }
+}
+*/
 function generateSequelize(config) {
   let tableName = config.table.tableName.toLowerCase();
   let columns = {};
   config.table.columns.forEach(column => {
     let entry = {};
-    deriveType(entry, column);
+    if (!column.key && !column.ref) {
+      deriveType(entry, column);
+    }
+    else {
+      entry.type = Sequelize.INTEGER;
+    }
     entry.allowNull = !column.mandatory;
 
-    if (column.default) {
-      if (column.type == "timestamp") {
-        entry.defaultValue = Sequelize.NOW;
-      }
-      else {
-        entry.defaultValue = column.default;
-      }
-    }
+    deriveDefaults(entry, column);
 
     if (column.lov) {
       entry.isIn = column.lov;
     }
 
-    entry.unique = column.unique;
-    entry.primaryKey = column.key;
-    entry.autoIncrement = column.key;
+    entry.unique = column.unique ? true : false;
+    entry.primaryKey = column.key ? true : false;
+
+    if (column.key) {
+      entry.autoIncrement = true;
+    }
 
     deriveReferences(entry, column);
 
@@ -235,30 +272,111 @@ function generateSequelize(config) {
     }
   });
 
-  console.log('============', tableName);
-  console.log(columns);
+  let model = sequelize.define(tableName, columns);
+  //console.log("Adding model", tableName);
+  //console.log(run(model, tableName));
+  sharedPersistenceMapping[tableName] = model;
+}
 
-  //sharedPersistenceMapping[tableName] = sequelize.define(tableName, columns);
+function deleteAPI(app, tableName) {
+  app.delete("/api/" + tableName + "/:id", function (req, res) {
+    //res.status(200).send([{ "A": "A" }, { "B": "B" }]);
+
+    console.log("Delete called for table ", tableName);
+
+    sharedPersistenceMapping[tableName].findById(req.params.id).then(inst => {
+      if (inst) {
+        inst.destroy()
+          .then(() => {
+            res.json({ "success": true });
+          })
+          .catch((e) => {
+            console.log(e);
+            res.status(400).send({ "success": false, "error": "Error failed because of " + e });
+          });
+      }
+      else {
+        res.status(404).send({ "success": false, "error": "Not Found" });
+      }
+    })
+  });
+}
+
+function selectAPI(app, tableName) {
+  app.get("/api/" + tableName, function (req, res) {
+
+    console.log("Get request called for table ", tableName);
+    //res.status(200).send([{ "A": "A" }, { "B": "B" }]);
+    // get the paging
+    // get the size
+    // get any ordering requests
+    let page = 0;
+    let size = 20;
+    let order = null;
+    let column = null;
+
+    let config = { limit: size, offset: page };
+
+    if (order && column) {
+      config.order = [[column, order]];
+    }
+    sharedPersistenceMapping[tableName].findAll(config).then(r => {
+      res.json(r);
+    })
+
+  });
+}
+
+function updateAPI(app, tableName) {
+  app.put("/api/" + tableName, function (req, res) {
+    console.log("Update api called for table", tableName, req.body);
+    sharedPersistenceMapping[tableName].upsert(req.body)
+      .then(i => {
+        res.json(i); // return as json instance
+      })
+      .catch(e => {
+        console.log(e);
+        res.status(400);
+      });
+  });
 }
 
 function generateRoutes(app, configuration) {
 
   configuration.forEach(conf => {
-    console.log(conf.table.tableName);
-    app.get("/api/" + conf.table.tableName.toLowerCase(), function (req, res) {
-      res.status(200).send([{ "A": "A" }, { "B": "B" }]);
-    });
-    app.delete("/api/" + conf.table.tableName.toLowerCase() + "/:id", function (req, res) {
-      res.status(200).send([{ "A": "A" }, { "B": "B" }]);
-    });
-    app.put("/api/" + conf.table.tableName.toLowerCase(), function (req, res) {
-      res.status(200).send([{ "A": "A" }, { "B": "B" }]);
-    });
+    let tableName = conf.table.tableName.toLowerCase();
+    selectAPI(app, tableName);
+    deleteAPI(app, tableName);
+    updateAPI(app, tableName);
     generateSequelize(conf);
+
+  });
+  sequelize.sync();
+
+  // get LOV based on model
+  // we will have query params for searching the lov with like
+  // run a query? perhaps we can do some client side caching here
+  app.get("/lov/:model", function (req, res) {
+
   });
 }
 // get will return paged data
 generateRoutes(app, getConfiguration());
+
+app.get('/tableProfiles', function (req, res) {
+  // get the table configurations
+
+  res.json(tableConfiguration);
+});
+
+app.get('/reportProfiles', function (req, res) {
+  // get report profile which defines the report 
+});
+
+app.get('/report/:name', function (req, res) {
+  // get report based on name
+  // note that the report is dynamically generated and returned
+})
 
 app.get('/', function (req, res) {
   console.log("Override");
